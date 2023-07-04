810e8b8135034d335c567d60df0c86d5977f9529
asharaya singh bhadauriya <118126127+Aashray24092000@users.noreply.github.com>
2023-07-04 13:32:17 +0530

    Update biggest_number.py

--- biggest_number.py
+++ biggest_number.py
@@ -8,3 +8,16 @@ def biggest(a,b,c):
    return bigger(a,bigger(b,c))
    
 print biggest(3,7,1)
+
+# another way of finding big number by function 
+
+def big(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+big(1000,200,300)
+
+#efficient code
